e2dfeb87fce934ae37e01b83197c91876a644ea7
YasirDulfi <estudiosenlavida@gmail.com>
2022-01-02 17:20:36 +0100

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
